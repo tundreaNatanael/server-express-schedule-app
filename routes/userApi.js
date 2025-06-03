2d71853beafd996896a9e6494a19f3aa36abca03
nathan.tundrea <natanael.tundrea@emanuel.ro>
2025-06-03 21:23:03 +0300

    user get prams > query

--- routes/userApi.js
+++ routes/userApi.js
@@ -4,7 +4,7 @@ import { literal } from "sequelize";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const id = Number(req.query?.id);
+  const id = Number(req.params?.id);
   if (!id || isNaN(id)) {
     return res.status(400).send({ message: "Invalid user ID" });
   }
